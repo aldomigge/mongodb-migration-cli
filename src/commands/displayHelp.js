b7ea8cfbaeace5ac5a7b0b5e99d6e384b5c3381b
Aldo B. Migge <aldobarbosa.dev@gmail.com>
2024-11-11 18:58:02 -0300

    feat: Adiciona os novos comandos para o display(comando) help. Corrige os comandos errados do help.

--- src/commands/displayHelp.js
+++ src/commands/displayHelp.js
@@ -1,11 +1,15 @@
 const displayHelp = () => {
   console.log(`
-    Uso: cli-migration [comando] [opções]
+    Uso: migration [comando] [opções]
 
     Comandos:
       create <name>         Cria um novo arquivo de migração
       run <up>              Rodar todas as migrações pendentes
       run <down>            Desfazer a última migração executada
+      register              Registra uma nova migração no banco de dados(Não executa a migração)
+      unregister            Remove o registro de uma migração do banco de dados(Não executa a migração)
+      safe-lock             Torna a migração como uma migração segura(Trava de segurança)
+      safe-unlock           Remove a trava de segurança de uma migração
       init                  Gera um arquivo de configuração migration.config.json
       test                  Testa a conexão com o banco de dados
       all-collections       Lista todas as coleções do banco de dados
@@ -16,6 +20,10 @@ const displayHelp = () => {
       --src <src>           Especifica o diretório de destino
       --file <file>         Especifica o arquivo de migração ou rollback
       --all                 Rollback de todas as migrações
+      --overwrite           Sobrescreve o arquivo de configuração existente
+      --safe                Adiciona a migração como segura
+      --force               Forçar a execução da migração
+      --version             Exibe a versão do pacote
 
     Exemplos:
       migration create create_users_table
@@ -25,6 +33,10 @@ const displayHelp = () => {
       migration run down
       migration run down --file NomeDoArquivo
       migration run down --all
+      migration register --file NomeDoArquivo
+      migration unregister --file NomeDoArquivo
+      migration safe-lock --file NomeDoArquivo
+      migration safe-unlock --file NomeDoArquivo
       migration init
       migration test
       migration all-collections
@@ -34,6 +46,3 @@ const displayHelp = () => {
 };
 
 export default displayHelp;
-
-// fix-import            Corrige a importação de módulos em arquivos de migração
-// migration fix-import
